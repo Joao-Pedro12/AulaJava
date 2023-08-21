d1278fc39c6580e9f7dd50d7e46cbd6cd143d8b2
João Pedro Silva Souza Nunes <126632669+Joao-Pedro12@users.noreply.github.com>
2023-08-21 20:29:31 -0300

    Add files via upload

---
+++ scan1.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class scan1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe um número");
+        int x = sc.nextInt();
+        System.out.println(x);
+    }
+
+}
