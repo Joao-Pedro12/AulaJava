b3a282cb357ac37b0049d6d4a2b13e022063ed81
João Pedro Silva Souza Nunes <126632669+Joao-Pedro12@users.noreply.github.com>
2023-08-21 19:53:16 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[]args) {
+        System.out.println("Hello World");
+
+        //FOR CRESCENTE E DECRECENTE:
+
+       // int i= 0;
+        //for (i = 0; i < 5; i++) {
+            //System.out.println(i);
+        //}
+
+        //int i=5;
+        //for(i=5;i>0;i--){
+            //System.out.println(i);
+        //}
+
+
+        //USANDO WHILE E DO WHILE:
+
+        //int contador = 0;
+        //while(contador<50){
+            //System.out.println("Repetição nº:"+ contador);
+            //contador++;
+        //}
+
+        //int i=0;
+        //do{
+            //System.out.println(i++);
+
+        //}while(i<=10);
+
+        //int i = 0;
+        //do{
+            //System.out.println(++i);
+        //}while(i<=10);
+    }
+
+}
